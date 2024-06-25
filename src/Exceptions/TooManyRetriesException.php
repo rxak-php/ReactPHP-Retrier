2db3d532027abd1ff206069c249f7e3a859f0cba
= <=>
2024-06-25 10:31:36 +0200

    Restore backwards compatibility, return all occurred exceptions

--- src/Exceptions/TooManyRetriesException.php
+++ src/Exceptions/TooManyRetriesException.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace Exan\Retrier\Exceptions;
 
 use Exception;
+use Throwable;
 
 class TooManyRetriesException extends Exception
 {
+    /**
+     * @param Throwable[] $exceptions
+     */
+    public function __construct(string $message = null, int $code = 0, public array $exceptions = [])
+    {
+        parent::__construct($message, $code, $exceptions[count($exceptions) - 1]);
+    }
 }
--- src/Retrier.php
+++ src/Retrier.php
@@ -7,17 +7,16 @@ namespace Exan\Retrier;
 use Exan\Retrier\Exceptions\TooManyRetriesException;
 use React\Promise\Promise;
 use React\Promise\PromiseInterface;
+use Throwable;
 
 class Retrier
 {
-    public static function retry(
-        int      $attempts,
-        callable $action
-    ): PromiseInterface
-    {
+    public static function attempt(int $attempts, callable $action): PromiseInterface {
         return new Promise(static function (callable $resolve, callable $reject) use ($attempts, $action) {
+            $exceptions = [];
             $retries = 0;
-            $shouldReject = static function () use (&$retries, $attempts) {
+            $shouldReject = static function (Throwable $e) use (&$retries, &$exceptions, $attempts) {
+                $exceptions[] = $e;
                 return ++$retries >= $attempts;
             };
 
@@ -28,16 +27,25 @@ class Retrier
                 $resolve,
                 $shouldReject,
                 $reject,
-                &$executeAction
+                &$executeAction,
+                &$exceptions
             ) {
                 $action($retries)
                     ->then($resolve)
-                    ->catch(static fn(\Throwable $e) => $shouldReject()
-                        ? $reject(new TooManyRetriesException(sprintf('Too many retries of #%d for exception: %s', $retries, $e->getMessage()), $e->getCode(), $e)) //TODO: maybe pass an array of occurred exceptions and not just the last one
+                    ->otherwise(static fn(\Throwable $e) => $shouldReject($e)
+                        ? $reject(new TooManyRetriesException(
+                            sprintf('Max attempts of %d reached', $retries),
+                            exceptions: $exceptions,
+                        ))
                         : $executeAction($action));
             };
 
             $executeAction($action);
         });
+    }
+
+    public function retry(int $attempts, callable $action): PromiseInterface
+    {
+        return self::attempt($attempts, $action);
     }
 }
